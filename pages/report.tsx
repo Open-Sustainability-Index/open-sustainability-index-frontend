57b1f9ca179b580889a1e61e3e90d1b84480fbf3
Tom Söderlund <tom@tomsoderlund.com>
2024-04-24 11:39:00 +0200

    yarn fix

--- pages/report.tsx
+++ pages/report.tsx
@@ -1,9 +1,9 @@
-import Script from "next/script"
+import Script from 'next/script'
 
 export default function Report () {
   return (
     <>
-      <Script type="text/javascript" src="https://form.jotform.com/jsform/240945423118050"></Script>
+      <Script type='text/javascript' src='https://form.jotform.com/jsform/240945423118050' />
     </>
   )
 }
