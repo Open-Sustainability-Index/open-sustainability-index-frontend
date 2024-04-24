8717a57f05c62327a127dda42c79e29f7f8b0b0e
Henric Malmberg <henric@tiburon.se>
2024-04-24 16:09:03 +0200

    use statsgrid on company details page

--- app/components/companies/CompanyDetails.tsx
+++ app/components/companies/CompanyDetails.tsx
@@ -3,13 +3,16 @@ import React from 'react'
 // import { Company } from 'graphql/__generated__/graphql'
 import CompanyChart from '../charts/CompanyChart'
 import { Company } from 'types/global'
+import { Box, Typography } from '@mui/material'
+import PageTopBanner from '../page/PageTopBanner'
+import StatsGrid from './StatsGrid'
 
 const CompanyDetails = ({ company, title }: { company: Company, title: string }): React.ReactElement => {
   return (
     <>
-      <h1>{title || company.company_name}</h1>
-      <p>{company?.industry}</p>
-      <p>{company?.emissions[0]?.scope_1}</p>
+      <PageTopBanner title='Company' description={company.company_name}>
+        <StatsGrid emission={company.emissions[company.emissions.length - 1]} />
+      </PageTopBanner>
       <CompanyChart company={company} />
     </>
   )
