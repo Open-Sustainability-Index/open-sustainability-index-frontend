4f958774ba4496e6e13fd979a49c45f6ca1f66fd
Tom Söderlund <tom@tomsoderlund.com>
2024-04-24 14:21:46 +0200

    PageTopBanner children

--- app/components/page/PageTopBanner.tsx
+++ app/components/page/PageTopBanner.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Box, Typography } from '@mui/material'
 
+interface PageTopBannerProps {
+  title?: string
+  description?: string
+  children?: React.ReactNode
+}
+
 const PageTopBanner = ({
   title = 'Welcome',
-  description = 'Welcome to a world of open, accessible, transparent, actionable sustainability data. We just launched.'
-}) => {
+  description = 'Welcome to a world of open, accessible, transparent, actionable sustainability data. We just launched.',
+  children
+}: PageTopBannerProps) => {
   return (
     <Box
       sx={{
@@ -20,6 +27,7 @@ const PageTopBanner = ({
       <Typography variant='h1' sx={{ marginTop: '8px' }}>
         {description}
       </Typography>
+      {children}
     </Box>
   )
 }
