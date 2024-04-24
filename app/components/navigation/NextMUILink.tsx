16b4c993a3d16b5e6fb08d961af0ccc006284084
Tom Söderlund <tom@tomsoderlund.com>
2024-04-24 14:56:56 +0200

    NextMUILink: the best of two worlds!

---
+++ app/components/navigation/NextMUILink.tsx
@@ -0,0 +1,18 @@
+import NextLink from 'next/link'
+import MUILink from '@mui/material/Link'
+
+interface NextMUILinkProps {
+  href: string
+  children: React.ReactNode
+  [key: string]: any
+}
+
+const NextMUILink = ({ href, children, ...props }: NextMUILinkProps) => {
+  return (
+    <NextLink href={href} passHref>
+      <MUILink {...props}>{children}</MUILink>
+    </NextLink>
+  )
+}
+
+export default NextMUILink
