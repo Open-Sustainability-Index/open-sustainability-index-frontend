cc360a39de21af9fd4f9d0223a0b0c587cba290a
Tom Söderlund <tom@tomsoderlund.com>
2024-04-25 16:47:22 +0200

    Industry not titlecase

--- app/services/industries.tsx
+++ app/services/industries.tsx
@@ -2,7 +2,7 @@ import { Industry, PageProps } from 'types/global'
 import { titleCase } from 'lib/strings'
 
 export const industriesPageProps = (industries: Industry[]): PageProps => {
-  const first3IndustryNames = industries.slice(0, 3).map((industry) => titleCase(industry.name)).join(', ')
+  const first3IndustryNames = industries.slice(0, 3).map((industry) => industry.name).join(', ')
   return {
     title: 'Global industry GHG emission data per year',
     description: `Get open-source global GHG emission data (scope 1/2/3) for industries such as ${first3IndustryNames}. Includes emissions per year, industry, country, and company intensity factors.`
