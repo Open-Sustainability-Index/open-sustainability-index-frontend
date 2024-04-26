ae7f517737729a3f283ecd4417b6c42e416dbb6a
Tom Söderlund <tom@tomsoderlund.com>
2024-04-26 16:28:44 +0200

    StatsGrid updates

--- app/components/companies/StatsGrid.tsx
+++ app/components/companies/StatsGrid.tsx
@@ -1,21 +1,24 @@
 import { Grid, Typography, Box } from '@mui/material'
+
 import { Emission } from 'types/global'
+import { parseFloatSpaces } from 'lib/strings'
 
 export default function StatsGrid ({ emission }: { emission: Emission }): React.ReactElement {
+  const intensity = (parseFloatSpaces(emission?.scope_1) / parseFloatSpaces(emission?.revenue)).toFixed(1)
   return (
     <Box sx={{ flexGrow: 1 }} pb={3}>
       <Grid container spacing={4}>
         {/* Each pair of number and label is a Grid item */}
-        <StatsBlock value={emission.year} description='Quick facts' />
-        <StatsBlock value={emission.scope_1} description='ton CO₂e' />
-        <StatsBlock value={emission.revenue} description='Revenue MUSD' />
-        <StatsBlock value={(emission.year / parseInt(emission.revenue)).toFixed(2)} description='t CO₂e / MUSD' />
+        <StatsBlock value={emission?.year} description='Quick facts' />
+        <StatsBlock value={emission?.scope_1} description='Emissions (ton CO₂e)' />
+        <StatsBlock value={emission?.revenue} description='Revenue (M USD)' />
+        <StatsBlock value={isNaN(intensity as any) ? undefined : intensity} description='Intensity (t CO₂e / M USD)' />
       </Grid>
     </Box>
   )
 };
 
-function StatsBlock ({ value, description }: { value: string | number, description: string }): React.ReactElement {
+function StatsBlock ({ value, description }: { value?: string | number, description: string }): React.ReactElement {
   return (
     <Grid item xs={6} sm={3} mt={6}>
       <Typography variant='h6' fontWeight='bold' color='primary'>
--- lib/strings.ts
+++ lib/strings.ts
@@ -1,4 +1,4 @@
 export const capitalizeFirstLetter = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1)
 export const titleCase = (str: string): string => str.replace(/(?:^|\s|[-"'([{])+\S/g, (c) => c.toUpperCase())
 
-export const parseFloatSpaces = (str: string): number => parseFloat(str.replace(/[^0-9.]/g, ''))
+export const parseFloatSpaces = (str: string): number => parseFloat(str?.replace(/[^0-9.]/g, ''))
