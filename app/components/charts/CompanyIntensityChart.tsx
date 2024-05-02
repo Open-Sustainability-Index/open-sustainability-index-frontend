492f33842abcaa47c9a0b488fdf2656bc2103bbf
Tom Söderlund <tom@tomsoderlund.com>
2024-05-02 14:43:31 +0200

    Chart styling

--- app/components/charts/CompanyIntensityChart.tsx
+++ app/components/charts/CompanyIntensityChart.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useTheme, useMediaQuery } from '@mui/material'
+import { useTheme, useMediaQuery, Theme } from '@mui/material'
 import {
   // ChartContainer
   ResponsiveChartContainer,
@@ -13,6 +13,8 @@ import {
   LinePlot,
   MarkPlot,
   legendClasses
+  // barElementClasses,
+  // lineElementClasses
 } from '@mui/x-charts'
 
 import { COLORS } from 'app/theme/theme'
@@ -61,7 +63,9 @@ const CompanyIntensityChart = ({ company }: { company: Company }): React.ReactEl
   ]
 
   // const Container = isSmallScreen ? ResponsiveChartContainer : ChartContainer
-  const sizingProps = isSmallScreen ? {} : { width: 500, height: 300 }
+  const sizingProps = isSmallScreen
+    ? { width: 380, height: 300, margin: { left: 70, right: 70 } }
+    : { width: 500, height: 500, margin: { left: 80, right: 100 } }
 
   return (
     <PaperCard>
@@ -82,8 +86,15 @@ const CompanyIntensityChart = ({ company }: { company: Company }): React.ReactEl
         {...sizingProps}
       >
         <BarPlot
-          leftAxis='emissionsAxis'
-          rightAxis='revenueAxis'
+          sx={(theme: Theme) => ({
+            // [`.${barElementClasses.root}`]: {
+            //   fill: 'red',
+            //   strokeWidth: 2
+            // },
+            rx: 6,
+            ry: 6,
+            width: 6
+          })}
         />
         <LinePlot />
         <MarkPlot />
@@ -91,22 +102,51 @@ const CompanyIntensityChart = ({ company }: { company: Company }): React.ReactEl
         <ChartsXAxis position='bottom' />
         <ChartsYAxis
           axisId='emissionsAxis'
-          label='Emissions'
+          label='t CO₂'
           position='left'
+          labelStyle={{
+            fontSize: '12px',
+            transform: 'rotate(0deg) translate(10px, -215px)',
+            textAlign: 'right',
+            fill: COLORS.GRAY_LIGHTER
+          }}
         />
         <ChartsYAxis
           axisId='revenueAxis'
-          label='Revenue'
+          label='M USD'
           position='right'
+          labelStyle={{
+            fontSize: '12px',
+            transform: 'rotate(0deg) translate(0px, -215px)',
+            textAlign: 'left',
+            fill: COLORS.GRAY_LIGHTER
+          }}
         />
         <ChartsLegend
           direction='row'
           itemMarkWidth={10}
           itemMarkHeight={10}
-          sx={(theme: any) => ({
-            [`.${legendClasses.root} text`]: {
-              fontSize: '10px'
+          slotProps={{
+            legend: {
+              labelStyle: {
+                fontSize: '12px',
+                fontWeight: 'normal',
+                fill: COLORS.GRAY_LIGHTER
+              },
+              markGap: 6,
+              itemGap: 20
             }
+          }}
+          sx={(theme: Theme) => ({
+            // [`.${legendClasses.root}`]: {
+            //   rx: 20,
+            //   ry: 20
+            // },
+            // [`.${legendClasses.root} text`]: {
+            //   fontSize: '10px'
+            // },
+            // rx: 20,
+            // ry: 20
           })}
         />
         <ChartsTooltip />
