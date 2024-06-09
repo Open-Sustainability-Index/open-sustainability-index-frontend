162782890ec21bc22e79ae9645dcb2c7a3a65dbd
Tom Söderlund <tom@tomsoderlund.com>
2024-06-09 21:19:18 +0200

    Title: “for OMX120 companies”

--- app/services/companies.tsx
+++ app/services/companies.tsx
@@ -42,9 +42,10 @@ interface CompaniesReturnProps {
 export const companiesPageProps = (companies: CompaniesCompany[], options?: ListEndpointParams): PageProps => {
   const first3CompanyNames = companies.slice(0, 3).map((company) => titleCase(company.company_name)).join(', ')
   const filterTags = options?.filters?.tags !== undefined ? `${options?.filters?.tags?.toUpperCase()} ` : ''
+  const forCompanies = `for ${filterTags}companies`
   return {
-    title: 'Company GHG emission data ' + (options?.sortSeoDescription ?? 'per industry and year'),
-    description: `Get open-source global GHG emission data (scope 1/2/3) for ${filterTags}companies such as ${first3CompanyNames}. Includes emissions per year, industry, country, and company intensity factors.`
+    title: 'Company GHG emission data ' + (options?.sortSeoDescription ?? (filterTags !== '' ? forCompanies : undefined) ?? 'per industry and year'),
+    description: `Get open-source global GHG emission data (scope 1/2/3) ${forCompanies} such as ${first3CompanyNames}. Includes emissions per year, industry, country, and company intensity factors.`
   }
 }
 
