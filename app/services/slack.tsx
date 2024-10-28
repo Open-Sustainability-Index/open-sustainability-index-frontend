787a131310114dda5861b949ef20a72f4b3add3c
Tom Söderlund <tom@tomsoderlund.com>
2024-10-28 17:26:16 +0100

    Slack: companyName

--- app/services/slack.tsx
+++ app/services/slack.tsx
@@ -4,10 +4,17 @@ import jsonToTSV from 'app/utils/jsonToTSV'
 // Initialize the Slack client with your bot token
 const client = new WebClient(process.env.SLACK_BOT_TOKEN)
 
+interface PostToSlackProps {
+  companyName: string
+  jsonData: Array<{ [key: string]: any }>
+  email: string
+  name?: string
+}
+
 // Send the message to a specific channel
-export async function postToSlack ({ jsonData, email, name }: { jsonData: Array<{ [key: string]: any }>, email: string, name?: string }): Promise<ChatPostMessageResponse | string | unknown | undefined> {
+export async function postToSlack ({ companyName, jsonData, email, name }: PostToSlackProps): Promise<ChatPostMessageResponse | string | unknown | undefined> {
   try {
-    const message = `${name as string ?? '(Someone)'} with email ${email ?? 'n/a'} just submitted the following data:\n\`\`\`${JSON.stringify(jsonData, null, 2)}\`\`\`\nHere is the CSV version for easy insert into Supabase:\n\`\`\`${jsonToTSV(jsonData, undefined, undefined, ',')}\`\`\``
+    const message = `${name as string ?? '(Someone)'} with email ${email ?? 'n/a'} just submitted the following data for *${companyName}*:\n\`\`\`${JSON.stringify(jsonData, null, 2)}\`\`\`\nHere is the CSV version for easy insert into Supabase:\n\`\`\`${jsonToTSV(jsonData, undefined, undefined, ',')}\`\`\``
 
     const response = await client.chat.postMessage({
       channel: '#data-submissions',
