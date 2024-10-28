0a2d0c0b63a3b8f2b2b18cf096978bd5cfdeee10
Tom Söderlund <tom@tomsoderlund.com>
2024-10-28 16:55:08 +0100

    Harder typing on 3x Emissions tables

--- app/components/companies/CompanyDetails.tsx
+++ app/components/companies/CompanyDetails.tsx
@@ -4,7 +4,7 @@ import { Container, Grid, Typography, Button } from '@mui/material'
 // import { Company } from 'graphql/__generated__/graphql'
 import CompanyIntensityChart from '../charts/CompanyIntensityChart'
 import CompanyTargetChart from '../charts/CompanyTargetChart'
-import { DataTableHorizontal, DataTableOnChangeFunction } from '../common/DataTable'
+import { DataTableHorizontal, DataTableOnChangeFunction, DataTableHeader } from '../common/DataTable'
 import { Company, Emission } from 'types/global'
 import { titleCase, formatAmount } from 'lib/strings'
 import PageTopBanner from '../page/PageTopBanner'
@@ -103,15 +103,59 @@ interface EmissionTableProps {
   onChange?: DataTableOnChangeFunction
 }
 
+type EmissionsDataTableHeader = Omit<DataTableHeader, 'field'> & { field: keyof Emission | 'just_header' }
+
+const revenueHeaders: EmissionsDataTableHeader[] = [
+  { field: 'year', label: 'Year', type: 'number', align: 'right' },
+  { field: 'revenue', label: 'Net Revenue (M USD)', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'source_revenue', label: 'Revenue Source', type: 'link', align: 'right' }
+]
+const emissionsOverviewHeaders: EmissionsDataTableHeader[] = [
+  { field: 'year', label: 'Year', type: 'number', align: 'right' },
+  { field: 'total_emission_market_based', label: 'Total Emissions - Market Based (t CO₂e)', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'emission_intensity', label: 'Emissions Intensity (t CO₂e / M USD)', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cradle_to_gate', label: 'Cradle-to-gate Intensity (t CO₂e / M USD)', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) }
+]
+const emissionsDetailsHeaders: EmissionsDataTableHeader[] = [
+  { field: 'year', label: 'Year', type: 'number', align: 'right' },
+  { field: 'scope_1', label: 'Scope 1', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+
+  { field: 'just_header', label: 'Scope 2', isHorizontalHeader: true, type: 'none', align: 'right' },
+  { field: 'scope_2_location_based', label: 'Scope 2 - Location Based', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'scope_2_market_based', label: 'Scope 2 - Market Based', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'scope_2_unknown', label: 'Scope 2 - Not specified', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+
+  { field: 'total_scope_3', label: 'Scope 3', isHorizontalHeader: true, type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+
+  { field: 'just_header', label: 'Upstream Emissions', isHorizontalHeader: true, type: 'none', align: 'right' },
+  { field: 'cat_1', label: '1. Purchased goods and services', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_2', label: '2. Capital goods', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_3', label: '3. Fuel- and energy-related activities', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_4', label: '4. Upstream transportation and distribution', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_5', label: '5. Waste generated in operations', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_6', label: '6. Business travel', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_7', label: '7. Employee commuting', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_8', label: '8. Upstream leased assets', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+
+  { field: 'just_header', label: 'Downstream Emissions', isHorizontalHeader: true, type: 'none', align: 'right' },
+  { field: 'cat_9', label: '9. Downstream transportation and distribution', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_10', label: '10. Processing of sold products', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_11', label: '11. Use of sold products', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_12', label: '12. End-of-life treatment of sold products', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_13', label: '13. Downstream leased assets', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_14', label: '14. Franchises', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+  { field: 'cat_15', label: '15. Investments', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+
+  { field: 'total_reported_emission_scope_1_2_3', label: 'Total Emissions', isHorizontalHeader: true, type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
+
+  { field: 'source_emission_link', label: 'Emission Source', type: 'link', align: 'right' }
+]
+
 export const RevenueTable = ({ emissions = [], onChange }: EmissionTableProps): React.ReactElement => {
   return (
     <DataTableHorizontal
       title='Revenue'
-      headers={[
-        { field: 'year', label: 'Year', type: 'number', align: 'right' },
-        { field: 'revenue', label: 'Net Revenue (M USD)', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'source_revenue', label: 'Revenue Source', type: 'link', align: 'right' }
-      ]}
+      headers={revenueHeaders}
       data={emissions}
       onChange={onChange}
     />
@@ -122,12 +166,7 @@ export const EmissionsOverviewTable = ({ emissions = [], onChange }: EmissionTab
   return (
     <DataTableHorizontal
       title='Emissions Overview'
-      headers={[
-        { field: 'year', label: 'Year', type: 'number', align: 'right' },
-        { field: 'total_emission_market_based', label: 'Total Emissions - Market Based (t CO₂e)', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'emission_intensity', label: 'Emissions Intensity (t CO₂e / M USD)', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cradle_to_gate', label: 'Cradle-to-gate Intensity (t CO₂e / M USD)', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) }
-      ]}
+      headers={emissionsOverviewHeaders}
       data={emissions}
       onChange={onChange}
     />
@@ -138,40 +177,7 @@ export const EmissionsDetailsTable = ({ emissions = [], onChange }: EmissionTabl
   return (
     <DataTableHorizontal
       title='Emissions Details'
-      headers={[
-        { field: 'year', label: 'Year', type: 'number', align: 'right' },
-        { field: 'scope_1', label: 'Scope 1', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-
-        { field: 'just_header', label: 'Scope 2', isHorizontalHeader: true, type: 'none', align: 'right' },
-        { field: 'scope_2_location_based', label: 'Scope 2 - Location Based', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'scope_2_market_based', label: 'Scope 2 - Market Based', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'scope_2_unknown', label: 'Scope 2 - Not specified', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-
-        { field: 'total_scope_3', label: 'Scope 3', isHorizontalHeader: true, type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-
-        { field: 'just_header', label: 'Upstream Emissions', isHorizontalHeader: true, type: 'none', align: 'right' },
-        { field: 'cat_1', label: '1. Purchased goods and services', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_2', label: '2. Capital goods', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_3', label: '3. Fuel- and energy-related activities', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_4', label: '4. Upstream transportation and distribution', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_5', label: '5. Waste generated in operations', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_6', label: '6. Business travel', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_7', label: '7. Employee commuting', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_8', label: '8. Upstream leased assets', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-
-        { field: 'just_header', label: 'Downstream Emissions', isHorizontalHeader: true, type: 'none', align: 'right' },
-        { field: 'cat_9', label: '9. Downstream transportation and distribution', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_10', label: '10. Processing of sold products', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_11', label: '11. Use of sold products', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_12', label: '12. End-of-life treatment of sold products', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_13', label: '13. Downstream leased assets', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_14', label: '14. Franchises', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-        { field: 'cat_15', label: '15. Investments', type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-
-        { field: 'total_reported_emission_scope_1_2_3', label: 'Total Emissions', isHorizontalHeader: true, type: 'number', align: 'right', format: (value: number): string => formatAmount(value) },
-
-        { field: 'source_emission_link', label: 'Emission Source', type: 'link', align: 'right' }
-      ]}
+      headers={emissionsDetailsHeaders}
       data={emissions}
       onChange={onChange}
     />
