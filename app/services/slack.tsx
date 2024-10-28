c26907b0995bb8850f108d134013735d88fcb346
Henric Malmberg <henric@tiburon.se>
2024-10-28 13:45:51 +0100

    post both json and csv to slack when user submits emissions

--- app/services/slack.tsx
+++ app/services/slack.tsx
@@ -1,16 +1,17 @@
 import { WebClient } from '@slack/web-api'
+import jsonToTSV from 'app/utils/jsonToTSV'
 import { error } from 'console'
 
 // Initialize the Slack client with your bot token
 const client = new WebClient(process.env.SLACK_BOT_TOKEN)
 
 // Send the message to a specific channel
-export async function postToSlack ({ jsonData, email, name }: { jsonData: object, email: string, name?: string }) {
+export async function postToSlack ({ jsonData, email, name }: { jsonData: { [key: string]: any }[], email: string, name?: string }) {
   try {
-    const message = `The user ${name as string} with email ${email} just submitted the following JSON:\n\`\`\`${JSON.stringify(jsonData, null, 2)}\`\`\``
+    const message = `The user ${name as string} with email ${email} just submitted the following JSON:\n\`\`\`${JSON.stringify(jsonData, null, 2)}\`\`\`\nHere is the CSV version for easy insert into Supabase:\n\`\`\`${jsonToTSV(jsonData,undefined,undefined,',')}\`\`\``
 
     const response = await client.chat.postMessage({
-      channel: '#data-submissions', // Replace with your channel ID
+      channel: '#data-submissions',
       text: message
     })
 
