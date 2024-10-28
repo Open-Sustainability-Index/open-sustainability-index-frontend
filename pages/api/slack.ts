67c84adbe03d5e1dc6c386ca32ddfad52be094eb
Tom Söderlund <tom@tomsoderlund.com>
2024-10-28 12:24:55 +0100

    Slack Git format

--- pages/api/slack.ts
+++ pages/api/slack.ts
@@ -5,6 +5,6 @@ export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const result = await postToSlack({jsonData: req.body.jsonData, email: req.body.email, name: req.body.name })
+  const result = await postToSlack({ jsonData: req.body.jsonData, email: req.body.email, name: req.body.name })
   return res.status(200).json(result)
 }
